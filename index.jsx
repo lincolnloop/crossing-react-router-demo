7aebef365f888a7a7bad6b69d67b9aeda24c5410
Brian Luft <brian@lincolnloop.com>
2015-08-03 16:35:23 -0700

    Annotate code with comments

--- index.jsx
+++ index.jsx
@@ -3,6 +3,28 @@ import { Route, Router, Link } from 'react-router'
 import History from 'react-router/lib/BrowserHistory'
 import Crossing from 'crossing'
 
+
+/**
+ * Define some URL paths and assign them to names
+ */
+const urlTable = {
+  'user:detail': '/user/:id',
+  'photos': '/photos'
+}
+
+/**
+ * Create an instance of Crossing, overriding the param format to be compatible with what react-router expects
+ */
+const urls = new Crossing(new RegExp(':([A-Za-z0-9-_%]{1,})'))
+urls.load(urlTable)
+
+/**
+ * Create two React components that will be displayed depending on which
+ * route is active.
+ *
+ * We're using the new ES6 class syntax to keep the code terse. Since these are pure components,
+ * all we really need is to pass React something with a `render` method.
+ */
 class User {
   render () {
     return (
@@ -17,6 +39,10 @@ class User {
   }
 }
 
+/**
+ * You can see in the `Link` elements, we're using the Crossing instance to obtain
+ * URL paths dynamically using the name and passing any appropriate parameters.
+ */
 class Photos {
   render () {
     return (
@@ -31,19 +57,16 @@ class Photos {
   }
 }
 
-const urls = new Crossing(new RegExp(':([A-Za-z0-9-_%]{1,})'))
-const urlTable = {
-  'user:detail': '/user/:id',
-  'photos': '/photos'
-}
-urls.load(urlTable)
-
+/**
+ * Define some routes for react-router. We're passing the patterns that include param placeholders.
+ */
 const routes = (
   <Route path='/'>
     <Route path={urlTable['user:detail']} component={User}/>
     <Route path={urlTable['photos']} component={Photos}/>
   </Route>
 )
+
 
 React.render(
   <Router history={new History}>{routes}</Router>,
